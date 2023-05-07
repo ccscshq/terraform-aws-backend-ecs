5085e7a5604c068fefaaad6ea10eacaafa164444
gotooooo <k.goto.123@gmail.com>
2023-05-07 19:06:35 +0900

    fix acm validation

--- acm.tf
+++ acm.tf
@@ -6,5 +6,5 @@ resource "aws_acm_certificate" "this" {
 
 resource "aws_acm_certificate_validation" "this" {
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = [for record in aws_route53_record.this : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.acm : record.fqdn]
 }
